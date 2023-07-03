be1858478a6f8a069bd402dbeb256079006c68d2
KazemHe <kazemhe1@gmail.com>
2023-07-03 15:27:16 +0300

    footer and appheader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import main from './Style/main.scss'
+import Footer from './cpm/Footer';
+import AppHeader from './cpm/AppHeader';
+import About from './cpm/About';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> nd save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+<AppHeader />
       </header>
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      </Routes>
+     <Footer />
     </div>
+    </Router>
   );
 }
 
