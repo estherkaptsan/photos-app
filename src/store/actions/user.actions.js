1b0807d14b25ec9e74193f8616427dcdf9db907b
KazemHe <kazemhe1@gmail.com>
2023-07-12 16:55:21 +0300

    handle loggedInUser

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -15,7 +15,7 @@ export function login(username, password) {
         return false;
       }
     } catch (error) {
-      console.log('error:', error);
+      console.log('Error:', error);
       return false;
     }
   };
@@ -26,7 +26,35 @@ export function logout() {
     try {
       dispatch({ type: 'LOGOUT' });
     } catch (error) {
-      console.log('error:', error);
+      console.log('Error:', error);
+    }
+  };
+}
+
+export function updateUser(user) {
+  return async (dispatch, getState) => {
+    try {
+      const { loggedInUser } = getState().user; // Get the logged-in user from the state
+      if (!loggedInUser) {
+        console.log('User not logged in');
+        return false;
+      }
+
+      // Check if the logged-in user matches the user to be updated
+      if (loggedInUser.name !== user.name) {
+        console.log('Unauthorized: Cannot update another user');
+        return false;
+      }
+
+      // Perform update logic using the userService
+      userService.update(user);
+
+      console.log('User updated successfully');
+      dispatch({ type: 'UPDATE_USER', user });
+      return true;
+    } catch (error) {
+      console.log('Error:', error);
+      return false;
     }
   };
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import thunk from 'redux-thunk'
 import { applyMiddleware, combineReducers, compose, legacy_createStore as createStore } from 'redux'
 import { pictureReducer } from './reducers/picture.reducer';
-// import { userReducer } from './reducers/user.reducer'
+import { userReducer } from './reducers/user.reducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const rootReducer = combineReducers({
     pictureModule: pictureReducer,
-    // userModule: userReducer
+    userModule: userReducer
 })
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
