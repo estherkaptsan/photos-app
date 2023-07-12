2c56823e31bd39f02caeb9ee39eb2fa09a31c96b
KazemHe <kazemhe1@gmail.com>
2023-07-12 23:01:24 +0300

    next and prev photo details

--- src/pages/PhotoDetail.jsx
+++ src/pages/PhotoDetail.jsx
@@ -1,13 +1,8 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { pictureService } from '../services/picture.service';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  loadPictures,
-  removePicture,
-  setFilterBy,
-  loadCategories,
-} from '../store/actions/picture.actions';
+import { loadPictures, } from '../store/actions/picture.actions';
 
 export function PhotoDetails(props) {
   console.log('photo details', props);
@@ -25,13 +20,13 @@ export function PhotoDetails(props) {
   }, [dispatch]);
 
   useEffect(() => {
-    loadPicturesToNextPrev();
+    loadPictureIds();
     loadPhoto();
   }, []);
 
-  async function loadPicturesToNextPrev() {
+  async function loadPictureIds() {
     try {
-      const ids = pictures.map((picture) => picture._id);
+        const ids = pictures.map((picture) => picture._id);
       console.log(ids);
       setPictureIds(ids);
     } catch (error) {
@@ -39,9 +34,18 @@ export function PhotoDetails(props) {
     }
   }
 
-  async function loadPhoto() {
+  async function loadPhoto(nextIndex) {
+    let photo = ''
     try {
-      const photo = await pictureService.getPictureById(params.id);
+
+      if (nextIndex)  {
+         photo = await pictureService.getPictureById(nextIndex);
+      } 
+        
+      else {
+
+         photo = await pictureService.getPictureById(params.id);
+      }
       setPhoto(photo);
     } catch (error) {
       console.log('error:', error);
@@ -52,21 +56,29 @@ export function PhotoDetails(props) {
     navigate('/gallery');
   }
 
-  function onNext() {
-    const currentIndex = pictureIds.indexOf(params.id);
+  async function onNext(currentId) {
+    const currentIndex = pictureIds.indexOf(currentId);
     const nextIndex = (currentIndex + 1) % pictureIds.length;
-    navigate(`/photo/${pictureIds[nextIndex]}`);
-    loadPhoto();
+    const nextPhoto = pictureIds[nextIndex]
+    navigate(`/photo/${nextPhoto}`);
+    loadPhoto(nextPhoto); // Load the photo first
   }
 
-  function onPrevious() {
-    const currentIndex = pictureIds.indexOf(params.id);
+
+
+
+  async function onPrevious(currentId) {
+    const currentIndex = pictureIds.indexOf(currentId);
     const previousIndex = (currentIndex - 1 + pictureIds.length) % pictureIds.length;
-    navigate(`/photo/${pictureIds[previousIndex]}`);
-    loadPhoto();
+
+    const prevPhoto= pictureIds[previousIndex]
+
+
+    navigate(`/photo/${prevPhoto}`);
+     loadPhoto(prevPhoto); // Load the photo first
+
   }
 
-  console.log('render');
 
   if (!photo) return <div>Loading...</div>;
   return (
@@ -75,9 +87,9 @@ export function PhotoDetails(props) {
       <section>
         <h3>{photo.title}</h3>
         <div className="photo-container">
-          <button onClick={onPrevious}>Previous</button>
+          <button onClick={() => onPrevious(photo._id)}>Previous</button>
           <img src={photo.imgUrl} />
-          <button onClick={onNext}>Next</button>
+          <button onClick={() => onNext(photo._id)}>Next</button>
         </div>
       </section>
     </section>
