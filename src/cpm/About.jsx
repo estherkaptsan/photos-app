244500b3da0dd50878e6d988916d2759b39a8e70
esther kaptsan <estherkaptsan@gmail.com>
2023-08-14 10:30:32 +0300

    about

--- src/cpm/About.jsx
+++ src/cpm/About.jsx
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <section className="about">
-      <h2 className="">About</h2>
-      <ul className="nav-links">
-        <li>+972 (0) 528 891567</li>
-        <li><Link to="https://www.instagram.com/gulitamuz_photography/">Instagram</Link></li>
-      </ul>
-
+    <section className="about-container">
+      <section className='contact'>
+        <h1>SEND ME A MESSAGE!</h1>
+        {/* <h2 className="">About</h2> */}
+        <ul className="nav-links">
+          <li>+972 (0) 528 891567</li>
+          <li>avigailtamuz@gmail.com</li>
+          <li><Link to="https://www.instagram.com/gulitamuz_photography/">Instagram</Link></li>
+        </ul>
+      </section>
+      <section>
+        <img src='https://res.cloudinary.com/dcwibf9o5/image/upload/v1691998110/nkuti5izrkwqwfzyhrfn.png'/>
+      </section>
     </section>
   );
 };
